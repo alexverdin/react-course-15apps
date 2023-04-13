0d22b6eb0a804de1a519c1b8e9db95c75cab057b
alexverdin <navarrov.alex@gmail.com>
2023-04-13 09:12:09 -0600

    finishing JS basis exercises

--- JavascriptBasis/js/22_fetchAsyncAwait.js
+++ JavascriptBasis/js/22_fetchAsyncAwait.js
@@ -1,11 +1,35 @@
-const url = " https://pokeapi.co/api/v2/pokemon/crobat";
+const url = " https://pokeapi.co/api/v2/pokemon/pikachu";
+const url2 = " https://jsonplaceholder.typicode.com/photos";
 
-const consultarAsyncAwait = async () => {
+const consultarAPI = async () => {
+  const inicio = performance.now();
+
   const response = await fetch(url);
-  console.log("after response...");
-  const result = await response;
-  console.log("after result...");
+  const result = await response.json();
+
+  const response2 = await fetch(url2);
+  const result2 = await response2.json();
+
+  const fin = performance.now();
+
+  console.log(`1er Async: ${fin - inicio}ms`);
   console.log(result);
+  console.log(result2);
 };
 
-consultarAsyncAwait();
+const consultarAPI2 = async () => {
+  const inicio = performance.now();
+
+  const [resp, resp2] = await Promise.all([fetch(url), fetch(url2)]);
+  const result = await resp.json();
+  const result2 = await resp2.json();
+
+  const fin = performance.now();
+
+  console.log(`2do Async: ${fin - inicio}ms`);
+  console.log(result);
+  console.log(result2);
+};
+
+consultarAPI();
+consultarAPI2();
